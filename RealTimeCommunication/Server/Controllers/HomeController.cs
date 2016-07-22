068565065bca893057ac9b13fbda3e0a60e8c9f1
rishikesht-cuelogic <rishikesh.tadka@cuelogic.co.in>
2016-07-22 17:13:38 +0530

    Added SignalR

--- RealTimeCommunication/Server/Controllers/HomeController.cs
+++ RealTimeCommunication/Server/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNet.SignalR;
+using Server.SignalR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,6 +13,14 @@ namespace Server.Controllers
     {
         public ActionResult Index()
         {
+            for(int i = 0; i < 5; i++)
+            {
+                var context = GlobalHost.ConnectionManager.GetHubContext<SignalRHub>();
+                context.Clients.All.Send("Admin", "stop the chat");
+                int milliseconds = 5000;
+                Thread.Sleep(milliseconds);
+            }
+
             ViewBag.Title = "Home Page";
 
             return View();
